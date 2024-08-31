735a85838118352f2a0375b5ddfdaf8d50b32e3d
ronaldobattisti <ronaldo.battisti@hotmail.com>
2024-08-31 18:29:12 -0300

    Started HTML classes

---
+++ Dynamic Development/1stExample/main.js
@@ -0,0 +1,15 @@
+console.log("Testing..");
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(a + b);
+//F8 to execute
+//Is case sensitive
+
+var namePerson = "Ronaldo";
+console.log(namePerson);
+
+//let needs to inicializate the variavle
+var name1;
+let name2 = "Ro";
+const pi = 3.14//constant
