393c09b1cebe982a369a49c6a2a6374980f64a2a
ronaldobattisti <ronaldo.battisti@hotmail.com>
2024-11-20 15:51:36 -0300

    End of subject Integrative Pracice in Software Development

---
+++ Integrative Practice in Software Development/7-Arrays/arrays.js
@@ -0,0 +1,10 @@
+let fruits = ["banana", "grape", "apple"];
+console.log(fruits[1]);
+
+let cars = new Array("golf", "gol", "montana");
+
+console.log(cars[2]);
+cars[3] = "amarok";
+
+console.log(cars);
+
